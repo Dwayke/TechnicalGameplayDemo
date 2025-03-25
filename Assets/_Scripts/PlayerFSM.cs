74744a409f9ea45b9f1886dc3f0bc31aa900efa5
Ahmad Dwayke <ahmad.dwayke@icloud.com>
2025-03-25 14:51:51 +0200

    Update PlayerFSM.cs

--- Assets/_Scripts/PlayerFSM.cs
+++ Assets/_Scripts/PlayerFSM.cs
@@ -1,7 +1,32 @@
+using Unity.IO.LowLevel.Unsafe;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PlayerFSM:MonoBehaviour
-
 {
-    
+    protected BasePlayerState _defaultState;
+    public BasePlayerState currentState;
+    private PlayerBrain _brain;
+
+    public virtual void Initialize()
+    {
+        _brain = GetComponent<PlayerBrain>();
+    }
+    public void UpdateState()
+    {
+        currentState?.UpdateState();
+    }
+    public void TransitionToState(BasePlayerState state)
+    {
+        if (currentState != null)
+        {
+            currentState?.ExitState();
+        }
+        currentState = state;
+        currentState.EnterState();
+    }
+    public void SetUpDefaultState()
+    {
+        //
+    }
 }
